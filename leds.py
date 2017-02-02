7c67a5edfb104adfd7b27cef43953d52d7838fa6
Jake Avery <jakeavery@jakeavery.me>
2017-02-02 18:34:11 -0500

    Got fade() written down but it's not working right

--- leds.py
+++ leds.py
@@ -9,8 +9,8 @@ for i in pins_used:                         # Sets up pins as outputs
     pi.set_mode(i, pigpio.OUTPUT)
 
 # Assign pins to strips and colors
-strip1 = {'red': 17, 'green': 27, 'blue': 22}
-strip2 = {'red': 18, 'green': 23, 'blue': 24}
+strip1 = (17, 27, 22)
+strip2 = (18, 23, 24)
 
 # Set frequency
 Hz = 800 
@@ -38,10 +38,38 @@ def breathe(color, time=1):
         print(level)
         sleep(freq)
         
-def fade(color1, color2, time=1):
+color1 = [255, 83, 13]
+color2 = [255, 13, 255]
+        
+def fade(color1=color1, color2=color2, time=1, strip=3):
     # Reset each color
     for i in pins_used:
         pi.set_PWM_dutycycle(i, 0)
+    strip1_levels = [0, 0, 0]
+    strip2_levels = [0, 0, 0]
+    # Determine steps needed
+    steps = time / freq
+    # Initialize delta tuple
+    delta = [0, 0, 0]
     # Get initial levels
-    
-    
+    for i in range(3):
+        strip1_levels[i] = pi.get_PWM_dutycycle(strip1[i])
+        strip2_levels[i] = pi.get_PWM_dutycycle(strip2[i])
+        # Determine amount to change each step
+        delta[i] = (color2[i] - color1[i]) / steps
+    # Loop
+    while True:
+        for i in range(3):
+            # Strip 1
+            if strip == 1 or strip == 3:
+                strip1_levels[i] += delta[i]
+                if strip1_levels[i] < min_level or strip1_levels[i] > max_level:
+                    delta[i] = -delta[i]
+                pi.set_PWM_dutycycle(strip1[i], strip1_levels[i])
+             # Strip 2
+            if strip == 2 or strip == 3:
+                strip2_levels[i] += delta[i]
+                if strip2_levels[i] < min_level or strip1_levels[i] > max_level:
+                    delta[i] = -delta[i]
+                pi.set_PWM_dutycycle(strip2[i], strip2_levels[i])   
+        sleep(freq)
