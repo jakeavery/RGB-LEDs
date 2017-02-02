8d88d1ef8864e1028ae5e579793242ce5d49a1c3
Jake Avery <jakeavery@jakeavery.me>
2017-02-02 17:52:32 -0500

    Put RGB led pins for each strip into dictionaries. Began working on fade()

--- leds.py
+++ leds.py
@@ -8,26 +8,24 @@ pins_used = (17, 27, 22, 18, 23, 24)
 for i in pins_used:                         # Sets up pins as outputs
     pi.set_mode(i, pigpio.OUTPUT)
 
-# Assign pins to colors
-R1 = 17
-G1 = 27
-B1 = 22
-R2 = 18
-G2 = 23
-B2 = 24
+# Assign pins to strips and colors
+strip1 = {'red': 17, 'green': 27, 'blue': 22}
+strip2 = {'red': 18, 'green': 23, 'blue': 24}
 
 # Set frequency
 Hz = 800 
 freq = 1 / Hz # in seconds
 
+# Set min and max levels
+min_level = 0
+max_level = 255
+
 def breathe(color, time=1):
     # Reset each color
     for i in pins_used:
         pi.set_PWM_dutycycle(i, 0)
-    # Set min and max levels 
+    # Get initial level 
     level = pi.get_PWM_dutycycle(color)       
-    min_level = 0
-    max_level = 255
     # Determine steps needed and amount to change each step
     steps = time / freq
     delta = (max_level - min_level) / steps
@@ -39,3 +37,11 @@ def breathe(color, time=1):
         pi.set_PWM_dutycycle(color, level)
         print(level)
         sleep(freq)
+        
+def fade(color1, color2, time=1):
+    # Reset each color
+    for i in pins_used:
+        pi.set_PWM_dutycycle(i, 0)
+    # Get initial levels
+    
+    
